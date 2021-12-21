480e570b9ae41e22fc2b7017a9bc3ac2d9dcbdea
Muhlise21 <sbstncnd@gmail.com>
2021-12-21 21:04:27 +0100

    with js galeria

---
+++ 21dec.ders/script.js
@@ -0,0 +1,27 @@
+const DATA = [
+    {
+    url: "./images/flw1.jpg", 
+    title : "Title 1",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+    },
+    {
+    url: "./images/flw2.jpeg", 
+    title : "Title 2",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+    },
+    {
+    url: "./images/flw3.jpeg", 
+    title : "Title 3",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+    },
+    {
+    url: "./images/flw4.jpeg", 
+    title : "Title 4",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+    },
+    {
+    url: "./images/flw5.jpeg", 
+    title : "Title 5",
+    description: "Some quick example text to build on the card title and make up the bulk of the card's content."
+    },
+]
